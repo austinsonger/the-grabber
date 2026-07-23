9d67380c509abf7050699c22617a0e567fc89adb
Austin Songer <asonger.pixel@gmail.com>
2026-07-22 23:08:46 -0500

    feat(jumpcloud): provider factory wiring for all 16 P0 collectors, dropped unused dates field

--- src/providers/jumpcloud/factory.rs
+++ src/providers/jumpcloud/factory.rs
@@ -4,15 +4,10 @@ use crate::evidence::{CsvCollector, EvidenceCollector, JsonCollector};
 use crate::providers::{CloudProvider, ProviderFactory};
 
 pub struct JumpCloudProviderFactory {
-    #[allow(dead_code)]
     client: JumpCloudClient,
     tenant_name: String,
-    #[allow(dead_code)]
     org_id: String,
-    #[allow(dead_code)]
     selected: Vec<String>,
-    #[allow(dead_code)]
-    dates: Option<(i64, i64)>,
 }
 
 impl JumpCloudProviderFactory {
@@ -21,15 +16,17 @@ impl JumpCloudProviderFactory {
         tenant_name: String,
         org_id: String,
         selected: Vec<String>,
-        dates: Option<(i64, i64)>,
     ) -> Self {
         Self {
             client,
             tenant_name,
             org_id,
             selected,
-            dates,
         }
+    }
+
+    fn has(&self, key: &str) -> bool {
+        self.selected.iter().any(|s| s == key)
     }
 }
 
@@ -43,13 +40,116 @@ impl ProviderFactory for JumpCloudProviderFactory {
     fn region(&self) -> &str {
         ""
     }
+
     fn csv_collectors(&self) -> Vec<Box<dyn CsvCollector>> {
-        Vec::new()
+        let mut v: Vec<Box<dyn CsvCollector>> = Vec::new();
+        if self.has("jumpcloud-users") {
+            v.push(Box::new(super::users::JumpCloudUsersCollector::new(
+                self.client.clone(),
+            )));
+        }
+        if self.has("jumpcloud-user-groups") {
+            v.push(Box::new(
+                super::user_groups::JumpCloudUserGroupsCollector::new(self.client.clone()),
+            ));
+        }
+        if self.has("jumpcloud-applications") {
+            v.push(Box::new(
+                super::applications::JumpCloudApplicationsCollector::new(self.client.clone()),
+            ));
+        }
+        if self.has("jumpcloud-mfa-factors") {
+            v.push(Box::new(
+                super::mfa_factors::JumpCloudMfaFactorsCollector::new(self.client.clone()),
+            ));
+        }
+        if self.has("jumpcloud-admin-roles") {
+            v.push(Box::new(
+                super::admin_roles::JumpCloudAdminRolesCollector::new(
+                    self.client.clone(),
+                    self.org_id.clone(),
+                ),
+            ));
+        }
+        if self.has("jumpcloud-systems") {
+            v.push(Box::new(super::systems::JumpCloudSystemsCollector::new(
+                self.client.clone(),
+            )));
+        }
+        if self.has("jumpcloud-disabled-users") {
+            v.push(Box::new(
+                super::disabled_users::JumpCloudDisabledUsersCollector::new(self.client.clone()),
+            ));
+        }
+        if self.has("jumpcloud-system-groups") {
+            v.push(Box::new(
+                super::system_groups::JumpCloudSystemGroupsCollector::new(self.client.clone()),
+            ));
+        }
+        v
     }
+
     fn json_collectors(&self) -> Vec<Box<dyn JsonCollector>> {
-        Vec::new()
+        let mut v: Vec<Box<dyn JsonCollector>> = Vec::new();
+        if self.has("jumpcloud-user-group-members") {
+            v.push(Box::new(
+                super::user_groups::JumpCloudUserGroupMembersCollector::new(self.client.clone()),
+            ));
+        }
+        if self.has("jumpcloud-policies") {
+            v.push(Box::new(super::policies::JumpCloudPoliciesCollector::new(
+                self.client.clone(),
+            )));
+        }
+        if self.has("jumpcloud-password-policy") {
+            v.push(Box::new(
+                super::password_policy::JumpCloudPasswordPolicyCollector::new(
+                    self.client.clone(),
+                    self.org_id.clone(),
+                ),
+            ));
+        }
+        if self.has("jumpcloud-session-policy") {
+            v.push(Box::new(
+                super::session_policy::JumpCloudSessionPolicyCollector::new(
+                    self.client.clone(),
+                    self.org_id.clone(),
+                ),
+            ));
+        }
+        if self.has("jumpcloud-system-group-members") {
+            v.push(Box::new(
+                super::system_groups::JumpCloudSystemGroupMembersCollector::new(
+                    self.client.clone(),
+                ),
+            ));
+        }
+        if self.has("jumpcloud-system-user-associations") {
+            v.push(Box::new(
+                super::system_user_associations::JumpCloudSystemUserAssociationsCollector::new(
+                    self.client.clone(),
+                ),
+            ));
+        }
+        v
     }
+
     fn evidence_collectors(&self) -> Vec<Box<dyn EvidenceCollector>> {
-        Vec::new()
+        let mut v: Vec<Box<dyn EvidenceCollector>> = Vec::new();
+        if self.has("jumpcloud-directory-insights") {
+            v.push(Box::new(
+                super::directory_insights::JumpCloudDirectoryInsightsCollector::new(
+                    self.client.clone(),
+                ),
+            ));
+        }
+        if self.has("jumpcloud-directory-alerts") {
+            v.push(Box::new(
+                super::directory_alerts::JumpCloudDirectoryAlertsCollector::new(
+                    self.client.clone(),
+                ),
+            ));
+        }
+        v
     }
 }
